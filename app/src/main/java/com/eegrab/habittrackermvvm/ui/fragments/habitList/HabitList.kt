36d7bf6399397d211f80d86d1c8da6b3f4bdfc67
abhinesh <abhinesh.c@eegrab.com>
2021-03-23 19:21:14 +0530

    able to add habit && delete all habits

--- app/src/main/java/com/eegrab/habittrackermvvm/ui/fragments/habitList/HabitList.kt
+++ app/src/main/java/com/eegrab/habittrackermvvm/ui/fragments/habitList/HabitList.kt
@@ -1,24 +1,70 @@
 package com.eegrab.habittrackermvvm.ui.fragments.habitList
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.eegrab.habittrackermvvm.R
+import com.eegrab.habittrackermvvm.data.models.Habit
+import com.eegrab.habittrackermvvm.ui.fragments.habitList.adapters.HabitListAdapter
+import com.eegrab.habittrackermvvm.ui.viewModels.HabitViewModel
 import kotlinx.android.synthetic.main.fragment_habit_list.*
 
 
 class HabitList : Fragment(R.layout.fragment_habit_list) {
 
-
+    private lateinit var habitList: List<Habit>
+    private lateinit var habitViewModel: HabitViewModel
+    private lateinit var adapter: HabitListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        //Adapter
+        adapter = HabitListAdapter()
+        rv_habits.adapter = adapter
+        rv_habits.layoutManager = LinearLayoutManager(context)
+
+        //ViewModel
+        habitViewModel = ViewModelProvider(this).get(HabitViewModel::class.java)
+
+        habitViewModel.getAllHabit.observe(viewLifecycleOwner, Observer {
+
+            adapter.setData(it)
+            habitList = it
+
+            if (it.isEmpty()) {
+                rv_habits.visibility = View.GONE
+                tv_emptyView.visibility = View.VISIBLE
+            } else {
+                rv_habits.visibility = View.VISIBLE
+                tv_emptyView.visibility = View.GONE
+            }
+        })
+
+        setHasOptionsMenu(true)
+
+        swipeToRefresh.setOnRefreshListener {
+            adapter.setData(habitList)
+            swipeToRefresh.isRefreshing = false
+        }
 
         fab_add.setOnClickListener {
           findNavController().navigate(R.id.action_habitList_to_createHabitItem)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.nav_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_delete -> habitViewModel.deleteAllHabits()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/eegrab/habittrackermvvm/ui/fragments/habitList/adapters/HabitListAdapter.kt
+++ app/src/main/java/com/eegrab/habittrackermvvm/ui/fragments/habitList/adapters/HabitListAdapter.kt
@@ -1,10 +1,18 @@
 package com.eegrab.habittrackermvvm.ui.fragments.habitList.adapters
 
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.eegrab.habittrackermvvm.R
 import com.eegrab.habittrackermvvm.data.models.Habit
+import com.eegrab.habittrackermvvm.ui.fragments.habitList.HabitList
+import com.eegrab.habittrackermvvm.ui.fragments.habitList.HabitListDirections
+import com.eegrab.habittrackermvvm.utils.Calculations
 import kotlinx.android.synthetic.main.recycler_habit_item.view.*
+import java.nio.file.Files.size
 
 class HabitListAdapter : RecyclerView.Adapter<HabitListAdapter.MyViewHolder>() {
 
@@ -16,6 +24,11 @@ class HabitListAdapter : RecyclerView.Adapter<HabitListAdapter.MyViewHolder>() {
         init {
             itemView.cv_cardView.setOnClickListener {
                 val position = adapterPosition
+                Log.d(TAG, "Item clicked at: $position ")
+                Log.d(TAG, "ID: ${habitList[position].id}")
+
+                val action = HabitListDirections.actionHabitListToUpdateHabitItem(habitList[position])
+                itemView.findNavController().navigate(action)
             }
         }
     }
@@ -24,15 +37,26 @@ class HabitListAdapter : RecyclerView.Adapter<HabitListAdapter.MyViewHolder>() {
         parent: ViewGroup,
         viewType: Int
     ): HabitListAdapter.MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_habit_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: HabitListAdapter.MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentHabit = habitList[position]
+
+        holder.itemView.iv_habit_icon.setImageResource(currentHabit.imageId)
+        holder.itemView.tv_item_title.text = currentHabit.habit_title
+        holder.itemView.tv_item_description.text = currentHabit.habit_description
+        holder.itemView.tv_timeElapsed.text = Calculations.calculateTimeBetweenDates(currentHabit.habit_startTime)
+        holder.itemView.tv_item_createdTimeStamp.text = "Since: ${currentHabit.habit_startTime}"
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return habitList.size
+    }
+
+    fun setData(habit: List<Habit>) {
+        this.habitList = habit
+        notifyDataSetChanged()
     }
 
 
